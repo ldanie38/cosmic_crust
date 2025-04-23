16b807c371be5237ed349e61c4cabe3507e18480
ldanie38 <ldanie38@asu.edu>
2025-04-23 00:42:28 -0400

    Updated MySQL connection to use ClearDB on Heroku

--- flask_app/config/mysqlconnection.py
+++ flask_app/config/mysqlconnection.py
@@ -1,24 +1,44 @@
+import os
 import pymysql.cursors
-# this class will give us an instance of a connection to our database
+from urllib.parse import urlparse
+
 class MySQLConnection:
     def __init__(self, db):
-        # change the user and password as needed
-        connection = pymysql.connect(host = 'localhost',
-                                    user = 'root', 
-                                    password = 'Stanislav24', 
-                                    db = db,
-                                    charset = 'utf8mb4',
-                                    cursorclass = pymysql.cursors.DictCursor,
-                                    autocommit = False)
-        # establish the connection to the database
+        # Check if we're running on Heroku (i.e. CLEARDB_DATABASE_URL exists)
+        clearDB_url = os.environ.get("CLEARDB_DATABASE_URL")
+        if clearDB_url:
+            # Parse the ClearDB URL to extract host, user, password, and db
+            url = urlparse(clearDB_url)
+            connection = pymysql.connect(
+                host=url.hostname,
+                port=url.port or 3306,
+                user=url.username,
+                password=url.password,
+                db=url.path.lstrip('/'),  # remove the leading '/'
+                charset='utf8mb4',
+                cursorclass=pymysql.cursors.DictCursor,
+                autocommit=False
+            )
+        else:
+            # Use local development settings
+            connection = pymysql.connect(
+                host='localhost',
+                user='root',
+                password='Stanislav24',
+                db=db,
+                charset='utf8mb4',
+                cursorclass=pymysql.cursors.DictCursor,
+                autocommit=False
+            )
+        # Establish the connection to the database
         self.connection = connection
+
     # the method to query the database
-    def query_db(self, query:str, data:dict=None):
+    def query_db(self, query: str, data: dict = None):
         with self.connection.cursor() as cursor:
             try:
                 query = cursor.mogrify(query, data)
                 print("Running Query:", query)
-     
                 cursor.execute(query)
                 if query.lower().find("insert") >= 0:
                     # INSERT queries will return the ID NUMBER of the row inserted
@@ -37,7 +57,8 @@ class MySQLConnection:
                 return False
             finally:
                 # close the connection
-                self.connection.close() 
+                self.connection.close()
+
 # connectToMySQL receives the database we're using and uses it to create an instance of MySQLConnection
 def connectToMySQL(db):
     return MySQLConnection(db)
