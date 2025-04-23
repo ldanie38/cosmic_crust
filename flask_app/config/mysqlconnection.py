77301555085a0f281ca8ff95df8eccbd890dadd7
ldanie38 <ldanie38@asu.edu>
2025-04-23 00:54:33 -0400

    Switch from ClearDB to JawsDB for remote MySQL

--- flask_app/config/mysqlconnection.py
+++ flask_app/config/mysqlconnection.py
@@ -4,23 +4,22 @@ from urllib.parse import urlparse
 
 class MySQLConnection:
     def __init__(self, db):
-        # Check if we're running on Heroku (i.e. CLEARDB_DATABASE_URL exists)
-        clearDB_url = os.environ.get("CLEARDB_DATABASE_URL")
-        if clearDB_url:
-            # Parse the ClearDB URL to extract host, user, password, and db
-            url = urlparse(clearDB_url)
+        # Try to get the database URL from JawsDB or ClearDB
+        db_url = os.environ.get("JAWSDB_URL") or os.environ.get("CLEARDB_DATABASE_URL")
+        if db_url:
+            url = urlparse(db_url)
             connection = pymysql.connect(
                 host=url.hostname,
                 port=url.port or 3306,
                 user=url.username,
                 password=url.password,
-                db=url.path.lstrip('/'),  # remove the leading '/'
+                db=url.path.lstrip('/'),
                 charset='utf8mb4',
                 cursorclass=pymysql.cursors.DictCursor,
                 autocommit=False
             )
         else:
-            # Use local development settings
+            # Use local settings for development
             connection = pymysql.connect(
                 host='localhost',
                 user='root',
@@ -30,10 +29,8 @@ class MySQLConnection:
                 cursorclass=pymysql.cursors.DictCursor,
                 autocommit=False
             )
-        # Establish the connection to the database
         self.connection = connection
 
-    # the method to query the database
     def query_db(self, query: str, data: dict = None):
         with self.connection.cursor() as cursor:
             try:
@@ -41,24 +38,18 @@ class MySQLConnection:
                 print("Running Query:", query)
                 cursor.execute(query)
                 if query.lower().find("insert") >= 0:
-                    # INSERT queries will return the ID NUMBER of the row inserted
                     self.connection.commit()
                     return cursor.lastrowid
                 elif query.lower().find("select") >= 0:
-                    # SELECT queries will return the data from the database as a LIST OF DICTIONARIES
                     result = cursor.fetchall()
                     return result
                 else:
-                    # UPDATE and DELETE queries will return nothing
                     self.connection.commit()
             except Exception as e:
-                # if the query fails the method will return FALSE
                 print("Something went wrong", e)
                 return False
             finally:
-                # close the connection
                 self.connection.close()
 
-# connectToMySQL receives the database we're using and uses it to create an instance of MySQLConnection
 def connectToMySQL(db):
     return MySQLConnection(db)
